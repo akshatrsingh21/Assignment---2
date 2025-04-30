e4962d0875c4d29760ed439a76608354dd87c3d1
akshatrsingh21 <139798410+akshatrsingh21@users.noreply.github.com>
2025-04-30 20:42:50 +0530

    Add files via upload

---
+++ Task7/Task7.js
@@ -0,0 +1,8 @@
+
+function startAnimation() {
+    document.getElementById("train").classList.remove("stopped");
+}
+
+function stopAnimation() {
+    document.getElementById("train").classList.add("stopped");
+}
